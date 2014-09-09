2f810423102c0ca87ff81abbbbafa01aad3ff227
Fabrício J. Barth <fabricio.barth@gmail.com>
2014-09-09 16:20:39 -0300

    Nova versao resultados

--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cp -rf scripts/figure results/
 cp scripts/*.html results/
-git checkout gh-pages
-git add results/
+git checkout master
+git add .
 git commit -m "Nova versao resultados"
+git checkout gh-pages
+git merge master
 git push origin gh-pages
