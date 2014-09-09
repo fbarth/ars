244c2c18e33772ddc753a90fcc161c2639f4e650
Fabrício J. Barth <fabricio.barth@gmail.com>
2014-09-09 16:32:05 -0300

    Nova versao resultados

--- publish.sh
+++ publish.sh
@@ -4,6 +4,7 @@ cp scripts/*.html results/
 git checkout master
 git add .
 git commit -m "Nova versao resultados"
+git push origin master
 git checkout gh-pages
 git merge master
 git push origin gh-pages
